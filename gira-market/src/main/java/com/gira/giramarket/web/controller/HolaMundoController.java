0960293abac42a4f6509343933ddd5d3765d7f09
Gael Ramos Laptop <esgaelramos@gmail.com>
2023-05-17 21:58:33 -0600

    refac and create structure project

--- gira-market/src/main/java/com/gira/giramarket/web/controller/HolaMundoController.java
+++ gira-market/src/main/java/com/gira/giramarket/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.gira.giramarket;
+package com.gira.giramarket.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
