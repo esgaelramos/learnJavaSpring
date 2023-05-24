dd179c6a89e7b5097531fb90eee45641fe6978f9
Gael Ramos <esgaelramos@gmail.com>
2023-05-24 15:47:45 -0600

    add entities for persistence and mapping the tables

---
+++ gira-market/src/main/java/com/gira/giramarket/persistance/entity/ComprasProductoPK.java
@@ -0,0 +1,33 @@
+package com.gira.giramarket.persistance.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable // Esta anotación indica que esta clase es una clase embebida en otra clase!!!
+public class ComprasProductoPK implements Serializable{
+
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+}
