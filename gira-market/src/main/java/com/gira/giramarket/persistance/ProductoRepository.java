c87e14c76576fa43b9d61c54cb72d4dc3cc9d1be
Gael Ramos <esgaelramos@gmail.com>
2023-05-24 19:47:26 -0600

    complement the repository

--- gira-market/src/main/java/com/gira/giramarket/persistance/ProductoRepository.java
+++ gira-market/src/main/java/com/gira/giramarket/persistance/ProductoRepository.java
@@ -1,16 +1,21 @@
 package com.gira.giramarket.persistance;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.gira.giramarket.persistance.crud.ProductoCrudRepository;
 import com.gira.giramarket.persistance.entity.Producto;
 
+// @Component no tan recomendado! mejor usar @Repository es más específico!
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
-        
         // sin casteo no funciona!
         // return productoCrudRepository.findAll();
 
@@ -24,6 +29,18 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidadStock, boolean estado) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock, estado);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 
 }
